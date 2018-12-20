f460e4006f8f0668cddee31dc21fb490f5aab51a
Artembytko <artem061@ukr.net>
2018-12-20 19:03:34 +0200

    fixed bugs

--- build/js/carousel.js
+++ build/js/carousel.js
@@ -14,9 +14,11 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
 
+
     if (n > slides.length){
       slideIndex = 1
     }
+
     if (n < 1){
         slideIndex=slides.length
     }
@@ -26,6 +28,7 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++){
       dots[i].className = dots[i].className.replace("active","");
     }
+
     slides[slideIndex-1].style.display = "inline-flex";
     dots[slideIndex-1].className+= " active";
 }
